eb1b7404ec70ea3d89062fdc53db6f71e74bd3db
Tu Huynh <huynhlethanhtu@gmail.com>
2025-09-10 07:42:27 +0700

    fix(minor): 024 minor change

--- 024.Generics/main.go
+++ 024.Generics/main.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-// As an example of a generic function, `SlicesIndex` takes a slice of any `comparable` 
-// type and an element of that type and returns the index of the first occurrence 
-// of `v` in `s`, or `-1` if not present. The `comparable` constraint means that 
-// we can compare values of this type with the `==` and `!=` operators. For a more 
-// thorough explanation of this type signature, see this blog post. Note that this 
+// As an example of a generic function, `SlicesIndex` takes a slice of any `comparable`
+// type and an element of that type and returns the index of the first occurrence
+// of `v` in `s`, or `-1` if not present. The `comparable` constraint means that
+// we can compare values of this type with the `==` and `!=` operators. For a more
+// thorough explanation of this type signature, see this blog post. Note that this
 // function exists in the standard library as slices.Index.
 func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
 	for i := range s {
@@ -17,7 +17,7 @@ func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
 	return -1
 }
 
-// As an example of a generic type, List is a singly-linked list with values of 
+// As an example of a generic type, List is a singly-linked list with values of
 // any type.
 type List[T any] struct {
 	head, tail *element[T]
@@ -28,7 +28,7 @@ type element[T any] struct {
 	val  T
 }
 
-// We can define methods on generic types just like we do on regular types, but 
+// We can define methods on generic types just like we do on regular types, but
 // we have to keep the type parameters in place. The type is `List[T]`, not `List`.
 func (lst *List[T]) Push(v T) {
 	if lst.tail == nil {
@@ -40,7 +40,7 @@ func (lst *List[T]) Push(v T) {
 	}
 }
 
-// `AllElements` returns all the `List` elements as a slice. In the next example 
+// `AllElements` returns all the `List` elements as a slice. In the next example
 // we’ll see a more idiomatic way of iterating over all elements of custom types.
 func (lst *List[T]) AllElements() []T {
 	var elems []T
@@ -52,8 +52,8 @@ func (lst *List[T]) AllElements() []T {
 
 func main() {
 	var s = []string{"foo", "bar", "zoo"}
-	// When invoking generic functions, we can often rely on type inference. Note 
-	// that we don’t have to specify the types for `S` and `E` when calling `SlicesIndex` 
+	// When invoking generic functions, we can often rely on type inference. Note
+	// that we don’t have to specify the types for `S` and `E` when calling `SlicesIndex`
 	// - the compiler infers them automatically.
 	fmt.Println("index of zoo:", SlicesIndex(s, "zoo"))
 
