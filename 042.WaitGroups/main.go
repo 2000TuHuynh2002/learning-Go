91d655eea9cd0ce6d242fd850e2d121db4a05d0c
Tu Huynh <huynhlethanhtu@gmail.com>
2025-10-17 22:19:56 +0700

    042. WaitGroups

---
+++ 042.WaitGroups/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// This is the function we’ll run in every goroutine.
+func worker(id int, wg *sync.WaitGroup) {
+	defer wg.Done() // Decrement the counter when the goroutine finishes
+	fmt.Printf("Worker %d starting\n", id)
+	// Sleep to simulate an expensive task.
+	time.Sleep(time.Second)
+	fmt.Printf("Worker %d done\n", id)
+}
+
+func main() {
+	// This `WaitGroup` is used to wait for all the goroutines launched here to finish. 
+	// Note: if a `WaitGroup` is explicitly passed into functions, it should be done 
+	// by pointer.
+	var wg sync.WaitGroup
+
+	// Launch several goroutines using WaitGroup.Go
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go worker(i, &wg)
+	}
+	// Block until all the goroutines started by `wg` are done. A goroutine is done 
+	// when the function it invokes returns.
+	wg.Wait() 
+	
+	// Note that this approach has no straightforward way to propagate errors from 
+	// workers. For more advanced use cases, consider using the errgroup package.
+}
